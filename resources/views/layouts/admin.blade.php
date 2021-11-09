41b2b2eb5b7fb64c8e56371888b979a42f4de0c8
Aldi2525 <tzzxuan1933@gmail.com>
2021-11-09 16:41:39 +0700

    konfigurasi santigarcor

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -59,7 +59,7 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2003-2021 <a href="https://wa.me/6289662747512">Aldi XSB</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.1.0
@@ -73,6 +73,7 @@
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
+
 
 <!-- jQuery -->
 <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,14 +1,14 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-dark navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="https://wa.me/6289662747512" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="https://wa.me/6289662747512" class="nav-link">Contact</a>
       </li>
     </ul>
 
@@ -40,7 +40,7 @@
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
+          <span class="badge badge-danger navbar-badge">1000</span>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <a href="#" class="dropdown-item">
@@ -49,11 +49,11 @@
               <img src="{{asset('assets/dist/img/user1-128x128.jpg')}}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  Brad Diesel
+                  Aceng Gans
                   <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                <p class="text-sm">Bagus Website nya</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> Baru saja</p>
               </div>
             </div>
             <!-- Message End -->
@@ -69,7 +69,7 @@
                   <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
                 </h3>
                 <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> Baru saja</p>
               </div>
             </div>
             <!-- Message End -->
@@ -81,11 +81,11 @@
               <img src="{{asset('assets/dist/img/user3-128x128.jpg')}}" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  Nora Silvester
+                  Dalgadot
                   <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                <p class="text-sm">Sini Belajar Online</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> Baru saja</p>
               </div>
             </div>
             <!-- Message End -->
@@ -131,5 +131,15 @@
           <i class="fas fa-th-large"></i>
         </a>
       </li>
+      <div class="nav-item">
+    <a class="nav-link" href="{{ route('logout') }}"
+     onclick="event.preventDefault();
+     document.getElementById('logout-form').submit();">
+      {{ __('Logout') }}
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+               @csrf
+  </form>
+  </div>
     </ul>
   </nav>
