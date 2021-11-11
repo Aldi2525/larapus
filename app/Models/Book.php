3cb494885ab4d1ebfbee2e1ca3d18266f00710fa
Aldi2525 <tzzxuan1933@gmail.com>
2021-11-11 16:45:28 +0700

    CRUD seeder laravel

---
+++ app/Models/Book.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $visible = ['title','author_id','amount','cover'];
+
+    protected $fillable = ['title','author_id','amount','cover'];
+
+    public $timestamps = true;
+
+    public function author()
+    {
+        // data model "Model" bisa memiliki oleh model "Author"
+        //melalui fk "author_id"
+        $this->belongsTo('App\Models\Author','author_id');
+    }
+}
